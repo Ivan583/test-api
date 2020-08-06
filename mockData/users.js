6914157eb36d70fcfefd0edd1e211dfd4070091c
tunikov <tunikov.spb@gmail.com>
2020-08-06 10:15:09 +0200

    init

---
+++ mockData/users.js
@@ -0,0 +1,23 @@
+const users = [
+  {
+    id: 1001,
+    name: "Matwey Abrikosov",
+    roles: ["admin"],
+  },
+  {
+    id: 1002,
+    name: "Feofan Bananov",
+    roles: ["user"],
+  },
+  {
+    id: 1003,
+    name: "Simon Vinogradov",
+    roles: ["user", "admin"],
+  },
+  {
+    id: 1004,
+    name: "Empidokl Grushin",
+    roles: ["user"],
+  },
+];
+module.exports = users;
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+const users = require("../mockData/users");
+/* GET users listing. */
+router.get("/:id", function (req, res, next) {
+  console.log(req.params);
+  const result = users.find((u) => u.id === parseInt(req.params.id));
+  console.log(result);
+  if (result) return res.json(result);
+  return res.status(404).send({ message: "Нет пользователя с данным ID" });
+});
+
+router.get("/", function (req, res, next) {
+  res.json({ users });
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+
+  if (!body.name)
+    res.status(400).send({
+      message:
+        "Имя пользователя - обязательное поле для создание нового пользователя",
+    });
+  res.status(201).send({
+    id: 1005,
+    name: req.body.name,
+    roles: ["user"],
+  });
+});
+
+module.exports = router;
